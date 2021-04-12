eb2a7a0baa6951c33030afec91259e9e235ffecb
IT Mokytojas <klase410@gmail.com>
2021-04-12 14:41:23 +0000

    suma

---
+++ suma/__init__.py
@@ -0,0 +1,21 @@
+import check50
+
+@check50.check()
+def exists():
+    """suma.cpp exists"""
+    check50.exists("suma.cpp")
+
+@check50.check(exists)
+def compiles():
+    """suma.cpp compiles"""
+    check50.run("g++ suma.cpp -lcrypt -lcs50 -lm -o suma").exit(0)
+
+@check50.check(compiles)
+def emma():
+    """responds to number 2"""
+    check50.run("./suma").stdin("2").stdin("5").stdout("7").exit()
+
+@check50.check(compiles)
+def rodrigo():
+    """responds to number 6"""
+    check50.run("./suma").stdin("6").stdin("8").stdout("14").exit()
